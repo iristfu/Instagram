10b11860e7629330fae98112f18dc46a1afc3f7e
Iris Fu <irisfu@fb.com>
2022-06-24 12:12:46 -0700

    Created compose view

---
+++ Instagram/View Controllers/ComposeViewController.h
@@ -0,0 +1,16 @@
+//
+//  ComposeViewController.h
+//  Instagram
+//
+//  Created by Iris Fu on 6/24/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
