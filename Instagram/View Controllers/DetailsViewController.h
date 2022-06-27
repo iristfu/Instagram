b9c014db9aad3280d1423090565038589c609f08
Iris Fu <irisfu@fb.com>
2022-06-26 17:03:25 -0800

    Set up details page and segue

---
+++ Instagram/View Controllers/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  Instagram
+//
+//  Created by Iris Fu on 6/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
